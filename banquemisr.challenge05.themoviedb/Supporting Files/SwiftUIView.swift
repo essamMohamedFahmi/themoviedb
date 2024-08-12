ac500ac1419692c16d1f80507e9f758f17df00cd
Essam Fahmy <essammohamedomran@gmail.com>
2024-08-12 20:27:47 +0300

    Refine initial structure

---
+++ banquemisr.challenge05.themoviedb/Supporting Files/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  banquemisr.challenge05.themoviedb
+//
+//  Created by Essam Fahmy on 12/08/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ banquemisr.challenge05.themoviedb/Supporting Files/TheMovieDBApp.swift
@@ -0,0 +1,20 @@
+//
+//  TheMovieDBApp.swift
+//  banquemisr.challenge05.themoviedb
+//
+//  Created by Essam Fahmy on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    
+    // MARK: - Body
+    
+    var body: some Scene {
+        WindowGroup {
+            SwiftUIView()
+        }
+    }
+}
